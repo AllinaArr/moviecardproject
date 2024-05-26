b0d35c709f24a03222dddff7ff6bc42d89323266
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-05-26 17:24:34 -0400

    I did a pop up

--- src/Components/AddMovieButton.js
+++ src/Components/AddMovieButton.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import "../index.css";
+
 function AddMovieButton({ addMovie, movie }) {
+  const [modal, setModal] = useState(false);
+  const [movieAdded, setMovieAdded] = useState(false);
+
   function handleClick() {
     console.log("clicked");
 
@@ -14,14 +20,35 @@ function AddMovieButton({ addMovie, movie }) {
       }),
     })
       .then((response) => response.json())
-      .then((data) => addMovie(data));
+      .then((data) => {
+        addMovie(data);
+        setMovieAdded(true);
+        setModal(true);
+      });
   }
 
+  function toggleModal() {
+    console.log("I checked a toggleModal");
+    setModal(!modal);
+  }
   return (
     <div className='divForBut'>
       <button className='overlay-button' onClick={handleClick}>
         Add to my watchlist
       </button>
+
+      {modal && (
+        <div className='modal'>
+          <div className='overlay'>
+            <div className='modal-content'>
+              {movieAdded && <h2>You added this movie to your watchlist</h2>}
+              <button className='close-modal' onClick={toggleModal}>
+                Done
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
