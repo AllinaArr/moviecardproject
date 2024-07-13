04bcc17be9d683de1f93b2248b26000b96364e2a
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-07-13 16:01:55 -0400

    //question abobut login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+import LogInOutComponent from "../Components/LogInOutComponent";
+
 function Login() {
-  return <h1>Logout</h1>;
+  return (
+    <Fragment>
+      <h1>Logout</h1>
+      <LogInOutComponent />
+    </Fragment>
+  );
 }
 
 export default Login;
