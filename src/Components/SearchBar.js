372deb6307b12985d734402b536cbb1e701c590a
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-07-13 18:15:29 -0400

    Change the style of Search Bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,26 +1,59 @@
+// function SearchBar({ searchValue, onChangeValue, onSearch }) {
+//   const handleSubmit = (e) => {
+//     console.log("Searching ... I am here");
+//     e.preventDefault();
+//     onSearch();
+//   };
+//   return (
+//     <div className='search-divider'>
+//       <form id='search-form' onSubmit={handleSubmit}>
+//         <div className='container'>
+//           <input
+//             id='search-input'
+//             type='text'
+//             value={searchValue}
+//             placeholder='Search for ...'
+//             onChange={(e) => onChangeValue(e.target.value)}
+//           />
+//           <button type='submit' className='container-button'>
+//             Search
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default SearchBar;
+import React from "react";
+import { TextField, Button, Box } from "@mui/material";
+
 function SearchBar({ searchValue, onChangeValue, onSearch }) {
   const handleSubmit = (e) => {
     console.log("Searching ... I am here");
     e.preventDefault();
     onSearch();
   };
+
   return (
-    <div className='search-divider'>
+    <Box className='search-divider' component='div'>
       <form id='search-form' onSubmit={handleSubmit}>
-        <div className='container'>
-          <input
+        <Box className='container'>
+          <TextField
             id='search-input'
             type='text'
             value={searchValue}
             placeholder='Search for ...'
             onChange={(e) => onChangeValue(e.target.value)}
+            variant='outlined'
+            size='small'
           />
-          <button type='submit' className='container-button'>
+          <Button type='submit' className='container-button'>
             Search
-          </button>
-        </div>
+          </Button>
+        </Box>
       </form>
-    </div>
+    </Box>
   );
 }
 
