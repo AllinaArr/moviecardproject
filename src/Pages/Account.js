593b5536e0c950306053b95a58bbe65e3665a156
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-07-13 17:02:41 -0400

    Account Page

---
+++ src/Pages/Account.js
@@ -0,0 +1,35 @@
+import SummaryOfAddedMovies from "../Components/SummaryOfAddedMovies";
+import { useEffect, useState } from "react";
+import AccordionAccount from "../Components/AccordionAccount";
+
+import MoviesAddedToAccount from "../Components/MoviesAddedToAccount";
+
+function Account({ deleteMovie }) {
+  const [movies, setMovies] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(movies);
+
+        setMovies(data);
+        setCount(data.length);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id='account-container'>
+        <div id='home-name'>
+          <h1>Account</h1>
+        </div>
+        {/* <SummaryOfAddedMovies count={count} /> */}
+        <AccordionAccount listOfMovies={movies} deleteMovie={deleteMovie} />
+      </div>
+    </div>
+  );
+}
+
+export default Account;
